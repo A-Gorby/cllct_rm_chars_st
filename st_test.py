86b6ac2a1e13f4e27845c81f7c339a35d58237d5
A-Gorby <andrey.a.gorbachev@mail.ru>
2024-09-30 10:22:49 +0300

    fst commit

--- st_test.py
+++ st_test.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame(
-    [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
-    ]
-)
+# df = pd.DataFrame(
+#     [
+#         {"command": "st.selectbox", "rating": 4, "is_widget": True},
+#         {"command": "st.balloons", "rating": 5, "is_widget": False},
+#         {"command": "st.time_input", "rating": 3, "is_widget": True},
+#     ]
+# )
 
-st.dataframe(df, use_container_width=True)
+# st.dataframe(df, use_container_width=True)
+
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
